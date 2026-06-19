fda94c84f6a1dd84f96fea27cd4a19b28c34ded7
Karan Yilmaz <113096967+BorsaKaplani178@users.noreply.github.com>
2026-06-19 18:36:03 +0300

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hash_mapS = {}
+        hash_mapT = {}
+
+        for i in s:
+            hash_mapS[i] = hash_mapS.get(i,0) + 1
+        
+        for j in t:
+            hash_mapT[j] = hash_mapT.get(j,0) + 1
+        
+        if hash_mapS == hash_mapT:
+            return True
+        
+        return False
+
